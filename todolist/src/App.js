f0cc3cfca35dc22b92bf8490be0bbf17c3dd47a9
nestornjrz <nestornjrz@hotmail.com>
2021-10-05 17:16:31 -0300

    Probando el check

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -7,9 +7,9 @@ import { CreateTodoButton } from './CreateTodoButton';
 //import './App.css';
 
 const todos = [
-  { text: "Cortar cebolla", completed: false },
+  { text: "Cortar cebolla", completed: true },
   { text: "Tomar el curso de intro a React", completed: false },
-  { text: "Llorar", completed: false },
+  { text: "Llorar", completed: true },
 ];
 
 function App() {
@@ -19,7 +19,11 @@ function App() {
       <TodoSearch />
       <TodoList>
         {todos.map((todo) => (
-          <TodoItem key={todo.text} text={todo.text}/>
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}
+          />
         ))}
       </TodoList>
       <CreateTodoButton />
